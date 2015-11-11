dc8d856e5f3ca91d7c26c32bd587e530df2b178b
Genie Jhang <geniejhang@majimak.com>
2015-11-12 06:38:20 +0900

    libCoreLinkDef.h: For Core dictionary

---
+++ source/Core/libCoreLinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class GETDecoder+;
+#pragma link C++ class GETFrameInfo+;
+#pragma link C++ class GETFileChecker+;
+
+#endif
