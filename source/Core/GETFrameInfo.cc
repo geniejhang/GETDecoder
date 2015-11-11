782a878b9d7c8e897bddc1b32f853ea352c0f0c1
Genie Jhang <geniejhang@majimak.com>
2015-11-12 06:35:40 +0900

    GETFrameInfo: Added stuff for GETCoboFrame.

--- source/Core/GETFrameInfo.cc
+++ source/Core/GETFrameInfo.cc
@@ -1,30 +1,49 @@
 #include "GETFrameInfo.hh"
 
 #include <cstdio>
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 ClassImp(GETFrameInfo)
 
-     void GETFrameInfo::SetFileID(UInt_t value)       { fFileID = value; }
-     void GETFrameInfo::SetEventID(UInt_t value)      { fEventID = value; }
-     void GETFrameInfo::SetEventTime(ULong64_t value) { fEventTime = value; }
-     void GETFrameInfo::SetDeltaT(UInt_t value)       { fDeltaT = value; }
-     void GETFrameInfo::SetStartByte(ULong64_t value) { fStartByte = value; }
-     void GETFrameInfo::SetEndByte(ULong64_t value)   { fEndByte = value; }
+        void  GETFrameInfo::SetDataID(UInt_t value)            { fDataID = value; }
+        void  GETFrameInfo::SetEventID(UInt_t value)           { fEventID = value; }
+        void  GETFrameInfo::SetEventTime(ULong64_t value)      { fEventTime = value; }
+        void  GETFrameInfo::SetDeltaT(UInt_t value)            { fDeltaT = value; }
+        void  GETFrameInfo::SetStartByte(ULong64_t value)      { fStartByte = value; }
+        void  GETFrameInfo::SetEndByte(ULong64_t value)        { fEndByte = value; }
+        void  GETFrameInfo::SetNextInfo(GETFrameInfo *pointer) { fNextInfo = pointer; }
 
-   UInt_t GETFrameInfo::GetFileID()                   { return fFileID; }
-   UInt_t GETFrameInfo::GetEventID()                  { return fEventID; }
-ULong64_t GETFrameInfo::GetEventTime()                { return fEventTime; }
-   UInt_t GETFrameInfo::GetDeltaT()                   { return fDeltaT; }
-ULong64_t GETFrameInfo::GetStartByte()                { return fStartByte; }
-ULong64_t GETFrameInfo::GetEndByte()                  { return fEndByte; }
+      UInt_t  GETFrameInfo::GetDataID()                   { return fDataID; }
+      UInt_t  GETFrameInfo::GetEventID()                  { return fEventID; }
+   ULong64_t  GETFrameInfo::GetEventTime()                { return fEventTime; }
+      UInt_t  GETFrameInfo::GetDeltaT()                   { return fDeltaT; }
+   ULong64_t  GETFrameInfo::GetStartByte()                { return fStartByte; }
+   ULong64_t  GETFrameInfo::GetEndByte()                  { return fEndByte; }
+GETFrameInfo *GETFrameInfo::GetNextInfo()                 { return fNextInfo; }
 
-   Bool_t GETFrameInfo::IsFill()                      { return (fStartByte != fEndByte); }
+      Bool_t  GETFrameInfo::IsFill()                      { return (fStartByte != fEndByte); }
 
 void GETFrameInfo::Clear(Option_t *) {
-  fFileID    = 0;
+  fDataID    = 0;
   fEventID   = 0;
   fEventTime = 0;
   fDeltaT    = 0;
   fStartByte = 0;
   fEndByte   = 0;
+  fNextInfo  = NULL;
+}
+
+void GETFrameInfo::Print() {
+  cout << "== GETFrameInfo =================" << endl;
+  cout << "   fDataID: " << fDataID << endl;
+  cout << "  fEventID: " << fEventID << endl;
+  cout << "fEventTime: " << fEventTime << endl;
+  cout << "   fDeltaT: " << fDeltaT << endl;
+  cout << "fStartByte: " << fStartByte << endl;
+  cout << "  fEndByte: " << fEndByte << endl;
+  cout << " fNextInfo: " << fNextInfo << endl;
+  cout << "=================================" << endl;
 }
--- source/Core/GETFrameInfo.hh
+++ source/Core/GETFrameInfo.hh
@@ -5,30 +5,35 @@
 
 class GETFrameInfo : public TObject {
   public:
-         void SetFileID(UInt_t value);
-         void SetEventID(UInt_t value);
-         void SetEventTime(ULong64_t value);
-         void SetDeltaT(UInt_t value);
-         void SetStartByte(ULong64_t value);
-         void SetEndByte(ULong64_t value);
+            void  SetDataID(UInt_t value);
+            void  SetEventID(UInt_t value);
+            void  SetEventTime(ULong64_t value);
+            void  SetDeltaT(UInt_t value);
+            void  SetStartByte(ULong64_t value);
+            void  SetEndByte(ULong64_t value);
+            void  SetNextInfo(GETFrameInfo *pointer);
 
-       UInt_t GetFileID();
-       UInt_t GetEventID();
-    ULong64_t GetEventTime();
-       UInt_t GetDeltaT();
-    ULong64_t GetStartByte();
-    ULong64_t GetEndByte();
+          UInt_t  GetDataID();
+          UInt_t  GetEventID();
+       ULong64_t  GetEventTime();
+          UInt_t  GetDeltaT();
+       ULong64_t  GetStartByte();
+       ULong64_t  GetEndByte();
+    GETFrameInfo *GetNextInfo();
 
-       Bool_t IsFill();
-         void Clear(Option_t * = "");
+          Bool_t  IsFill();
+            void  Clear(Option_t * = "");
+
+            void  Print();
 
   private:
-       UInt_t fFileID;
-       UInt_t fEventID;
-    ULong64_t fEventTime;
-       UInt_t fDeltaT;
-    ULong64_t fStartByte;
-    ULong64_t fEndByte;
+          UInt_t  fDataID;
+          UInt_t  fEventID;
+       ULong64_t  fEventTime;
+          UInt_t  fDeltaT;
+       ULong64_t  fStartByte;
+       ULong64_t  fEndByte;
+    GETFrameInfo *fNextInfo;
 
   ClassDef(GETFrameInfo, 1)
 };
