7529dac944784941577f21cf007e29787e11b2cb
Genie Jhang <changj@frib.msu.edu>
2024-05-06 16:45:47 +0000

    Create one single library

--- source/libGETDecoderLinkDef.h
+++ source/libGETDecoderLinkDef.h
@@ -4,6 +4,11 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class RingItemHeader+;
+#pragma link C++ class RingItemBodyHeader+;
+#pragma link C++ class RingStateChangeItem+;
+#pragma link C++ class RingPhysicsEventItem+;
+
 #pragma link C++ class GETHeaderBase+;
 #pragma link C++ class GETBasicFrameHeader+;
 #pragma link C++ class GETLayerHeader+;
@@ -13,5 +18,11 @@
 #pragma link C++ class GETCoboFrame+;
 #pragma link C++ class GETLayeredFrame+;
 #pragma link C++ class GETMutantFrame+;
+
+#pragma link C++ class GETFileChecker+;
+#pragma link C++ class GETMath+;
+
+#pragma link C++ class GETDecoder+;
+#pragma link C++ class GETFrameInfo+;
 
 #endif
